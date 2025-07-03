ea78ed7440e969a091412ce67d2aba8cbfee2be3
Mounika Singampalli <134005998+mouni131199@users.noreply.github.com>
2025-07-03 12:12:10 +0530

    updation

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 from flask_pymongo import PyMongo
 from datetime import datetime
@@ -57,5 +58,7 @@ def logout():
     session.clear()
     return redirect('/')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
